7fd7f41aae6068bde858bc91a69d3846aa89d687
JakubKrz <krzywon.jakub1@gmail.com>
2026-08-22 20:42:48 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use winit::event_loop::EventLoop;
+
+pub mod app;
+pub mod state;
+use crate::app::App;
+
+pub fn run() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = App::new();
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
+}
